b42d6b2dd25010a0212ee5d6cf402bd79122135a
Muhammad Mukhtar Suleiman <muhammadmukhtarsuleiman@gmail.com>
2024-10-17 10:42:57 +0100

    changed user to getUser

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -61,15 +61,16 @@ const Navbar = async () => {
 
                 <div className='h-8 w-px bg-zinc-200 hidden sm:block' />
 
-                <Link
-                  href='/configure/upload'
-                  className={buttonVariants({
-                    size: 'sm',
-                    className: 'hidden sm:flex items-center gap-1',
-                  })}>
-                  Create case
-                  <MoveRight className='ml-1.5 h-5 w-5' />
-                </Link>
+                  <Link
+                    href='/configure/upload'
+                    className={buttonVariants({
+                      size: 'sm',
+                      className: 'hidden sm:flex items-center gap-1',
+                    })}>
+                    Create case
+                    <MoveRight className='ml-1.5 h-5 w-5' />
+                  </Link>
+
               </>
             )}
           </div>
